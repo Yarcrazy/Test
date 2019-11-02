81baf5516cf86a50b3944612cf5348eb4fb22377
Yarcrazy <yacrazy@yandex.ru>
2019-11-02 19:22:52 +0700

    убрал gii и debug

--- config/web.php
+++ config/web.php
@@ -61,16 +61,16 @@ if (YII_ENV_DEV) {
   // configuration adjustments for 'dev' environment
   $config['bootstrap'][] = 'debug';
   $config['modules']['debug'] = [
-    'class' => 'yii\debug\Module',
+    // 'class' => 'yii\debug\Module',
     // uncomment the following to add your IP if you are not connecting from localhost.
-    'allowedIPs' => ['*'],
+    // 'allowedIPs' => ['*'],
   ];
 
   $config['bootstrap'][] = 'gii';
   $config['modules']['gii'] = [
-    'class' => 'yii\gii\Module',
+    // 'class' => 'yii\gii\Module',
     // uncomment the following to add your IP if you are not connecting from localhost.
-    'allowedIPs' => ['*'],
+    // 'allowedIPs' => ['*'],
   ];
 }
 
